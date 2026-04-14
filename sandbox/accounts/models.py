bf0043852dd9838a7d63ef4d72922d86cf5919e8
ilya3052 <ipopadin5@gmail.com>
2026-04-14 16:41:00 +0300

    добавление связи пользователя и группы

--- sandbox/accounts/models.py
+++ sandbox/accounts/models.py
@@ -1,21 +1,21 @@
-from datetime import timedelta
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
 from sandbox import settings
 
+
 def default_expires_at():
     return timezone.now() + settings.SHORT_TOKEN_LIFETIME
 
-# Create your models here.
+
 class CustomUser(AbstractUser):
     tg_link = models.CharField(max_length=255, blank=True, null=True)
     tg_id = models.IntegerField(blank=True, null=True)
     vk_link = models.CharField(max_length=255, blank=True, null=True)
     vk_id = models.IntegerField(blank=True, null=True)
     is_email_confirmed = models.BooleanField(default=False)
+    groups = models.ManyToManyField('Group')
 
 
 class TelegramToken(models.Model):
@@ -25,13 +25,15 @@ class TelegramToken(models.Model):
     refresh_token = models.CharField(max_length=512)
     expires_at = models.DateTimeField(default=default_expires_at)
 
+
 class VKTokens(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, unique=True)
     refresh_vk_token = models.CharField(max_length=512)
     access_vk_token = models.CharField(max_length=512)
-    id_vk_token = models.CharField(max_length=512)
+    id_vk_token = models.TextField()
     expires_in = models.IntegerField()
     added_at = models.DateTimeField(default=default_expires_at)
+
 
 class EmailActivate(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, unique=True)
