d91c6b63d286d7ca9b6f63293297e26efa53f827
ilya3052 <ipopadin5@gmail.com>
2026-04-14 16:40:11 +0300

    создание разрешения для изменения только админам, но чтения всем

--- sandbox/accounts/permissions.py
+++ sandbox/accounts/permissions.py
@@ -1,4 +1,10 @@
-from icecream import ic
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            (request.user and
+            request.user.is_staff)
+        )
