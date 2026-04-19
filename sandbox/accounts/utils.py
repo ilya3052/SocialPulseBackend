f61948bc589a14a0b3a56417fb973dad950cfb16
ilya3052 <ipopadin5@gmail.com>
2026-04-19 22:30:25 +0300

    добавление enum-а платформ

--- sandbox/accounts/utils.py
+++ sandbox/accounts/utils.py
@@ -9,6 +9,36 @@ class Status(IntEnum):
     ContactsNotFound = 2
     Error = 3
 
+class Platforms(Enum):
+    VK = (1, 'vk')
+    TG = (2, 'tg')
+
+    def __init__(self, code: int, alias: str):
+        self.code = code
+        self.alias = alias
+
+    @classmethod
+    def _missing_(cls, value: Union[int, str]):
+        if isinstance(value, str):
+            val = value.strip().lower()
+            for member in cls:
+                if member.alias == val:
+                    return member
+        if isinstance(value, int):
+            return cls._value2member_map_.get(value)
+        raise ValueError(f"{value} некорректный параметр для {cls.__name__}")
+
+    @property
+    def id(self) -> int:
+        return self.code
+
+    @property
+    def name(self) -> str:
+        return self.alias
+
+    def __int__(self) -> int:
+        return self.code
+
 
 def generate_short_token() -> str:
     url_token = secrets.token_urlsafe(48)
