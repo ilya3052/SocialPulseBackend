db14bba7a7f0968b16902a66720b63b076dc47fd
ilya3052 <ipopadin5@gmail.com>
2026-04-21 21:44:17 +0300

    добавление url для сервиснвых аккаунтов

--- src/admin_panel/urls.py
+++ src/admin_panel/urls.py
@@ -4,7 +4,10 @@ from admin_panel.views import PlatformsView, ServiceAccountsView, SummaryAdminPa
 
 urlpatterns = [
     path('platforms/', PlatformsView.as_view({"get": "list", "post": "create"}), name='platforms'),
-    path('service-accounts/<str:platform>/', ServiceAccountsView.as_view({"get": "retrieve"}), name='add-service-account'),
     # главная страница панели
     path('summary/', SummaryAdminPanelView.as_view(), name='summary-admin-panel'),
+    path('service-accounts/summary/', ServiceAccountsView.as_view({"get": "list"}), name='get-service-accounts-info'),
+    path('service-accounts/<str:platform>/', ServiceAccountsView.as_view({"get": "retrieve"}),
+         name='get-service-account'),
+
 ]
