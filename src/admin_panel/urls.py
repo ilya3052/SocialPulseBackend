45c79df405abcb788174485b53ba9724b3623217
ilya3052 <ipopadin5@gmail.com>
2026-04-21 21:02:18 +0300

    добавление url для summary-страницы

--- src/admin_panel/urls.py
+++ src/admin_panel/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from admin_panel.views import PlatformsView, ServiceAccountsView, SummaryAdminPanelView, LoadingOnServiceAccounts
+from admin_panel.views import PlatformsView, ServiceAccountsView, SummaryAdminPanelView
 
 urlpatterns = [
     path('platforms/', PlatformsView.as_view({"get": "list", "post": "create"}), name='platforms'),
-    path('service-accounts/', ServiceAccountsView.as_view({"get": "list", "post": "create"}), name='add-service-account'),
     path('service-accounts/<str:platform>/', ServiceAccountsView.as_view({"get": "retrieve"}), name='add-service-account'),
-    path('service-accounts/loading', LoadingOnServiceAccounts.as_view(), name='service-account-loading'),
     # главная страница панели
     path('summary/', SummaryAdminPanelView.as_view(), name='summary-admin-panel'),
 ]
