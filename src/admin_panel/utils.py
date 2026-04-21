f5b76a6df236d0245f9aee2589009904f13065ca
ilya3052 <ipopadin5@gmail.com>
2026-04-21 21:02:48 +0300

    создание функций для сбора информация summary-страницы

--- src/admin_panel/utils.py
+++ src/admin_panel/utils.py
@@ -1,17 +1,53 @@
-from icecream import ic
+from django.db.models import Q
+from django.db.models.aggregates import Count
 
 from admin_panel.models import ServiceAccount
 
 
-def get_account_with_minimum_loaded(accounts) -> ServiceAccount:
-    stats = [(account, len(account.groups.all())) for account in accounts]
-    return min(
-        stats,
-        key=lambda item: (item[1], item[0].name)
-    )[0]
+def get_group_aggregated_info():
+    from accounts.models import Group
+    result = Group.objects.all().aggregate(
+        vk_count=Count('id', filter=Q(platform__alias='VK')),
+        tg_count=Count('id', filter=Q(platform__alias='TG')))
+
+    return {
+        "vk_count": result.get('vk_count'),
+        "tg_count": result.get('tg_count'),
+    }
 
 
-def get_accounts_with_minimum_and_maximum_loaded(accounts):
-    stats = [(account, len(account.groups.all())) for account in accounts]
-    return (min(stats, key=lambda item: (item[1], item[0].name))[0],
-            max(stats, key=lambda item: (item[1], item[0].name))[0])
+def get_service_accounts_aggregated_info():
+    result = ServiceAccount.objects.all().aggregate(
+        vk_count=Count('id', filter=Q(platform__alias='VK')),
+        tg_count=Count('id', filter=Q(platform__alias='TG')))
+
+    return {
+        "vk_count": result.get('vk_count'),
+        "tg_count": result.get('tg_count'),
+    }
+
+
+def get_service_accounts_loading():
+    accounts = (
+        ServiceAccount.objects.all()
+        .prefetch_related('groups')
+        .annotate(
+            groups_count=Count('groups')
+        )
+    )
+
+    min_l_acc: ServiceAccount = accounts.order_by('groups_count', 'name').first()
+    max_l_acc: ServiceAccount = accounts.order_by('-groups_count', '-name').first()
+
+    return {
+        "min": {
+            "id": min_l_acc.id,
+            "name": min_l_acc.name,
+            "count": min_l_acc.groups_count
+        },
+        "max": {
+            "id": max_l_acc.id,
+            "name": max_l_acc.name,
+            "count": max_l_acc.groups_count
+        },
+    }
