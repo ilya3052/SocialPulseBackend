a6cf1c0857914ba166892326c3da75e9569ffab9
ilya3052 <ipopadin5@gmail.com>
2026-04-23 19:27:11 +0300

    создание приложения social_admin для административной панели

--- src/social_admin/views/admin_panel_views.py
+++ src/social_admin/views/admin_panel_views.py
@@ -3,14 +3,13 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from admin_panel.utils import get_group_aggregated_info, get_service_accounts_aggregated_info, \
-    get_service_accounts_loading
-
 
 class SummaryAdminPanelView(APIView):
     permission_classes = [IsAdminUser]
 
     def get(self, request, *args, **kwargs):
+        from service_accounts.services import get_service_accounts_aggregated_info, get_service_accounts_loading
+        from social_entities.services import get_group_aggregated_info
         group_data = get_group_aggregated_info()
         service_account_data = get_service_accounts_aggregated_info()
         service_account_loading = get_service_accounts_loading()
