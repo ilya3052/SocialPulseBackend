d197250c6baac9dc478a69a0be11ea9ec3c8eb44
ilya3052 <ipopadin5@gmail.com>
2026-04-26 15:14:30 +0300

    добавление поля app_id для вк и изменение параметров поля номера телефона

--- src/service_accounts/models.py
+++ src/service_accounts/models.py
@@ -4,12 +4,13 @@ from django.db import models
 class ServiceAccount(models.Model):
     name = models.CharField(max_length=128)
     is_activated = models.BooleanField(default=False)
+    app_id = models.CharField(unique=True, db_index=True, null=True, blank=True)
     platform = models.ForeignKey('social_entities.Platform', on_delete=models.CASCADE)
 
 
 class ServiceAccountData(models.Model):
     service_key = models.CharField(max_length=256, blank=True, null=True)
     protected_key = models.CharField(max_length=256, blank=True, null=True)
-    phone_number = models.CharField(max_length=16, blank=True, null=True)
+    phone_number = models.CharField(max_length=11, blank=True, null=True, unique=True)
     session_path = models.CharField(max_length=256, blank=True, null=True)
     account = models.OneToOneField('ServiceAccount', on_delete=models.CASCADE, related_name='data')
