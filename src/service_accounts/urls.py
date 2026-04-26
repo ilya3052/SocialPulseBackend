7ce83886d9b7e337eb8421b542e9158642644574
ilya3052 <ipopadin5@gmail.com>
2026-04-26 15:14:51 +0300

    изменение url получения всех сервисных аккаунтов

--- src/service_accounts/urls.py
+++ src/service_accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from service_accounts.views import ServiceAccountsView
 
 urlpatterns = [
-    path('summary/', ServiceAccountsView.as_view({"get": "list"}), name='get-service-accounts-info'),
+    path('all/', ServiceAccountsView.as_view({"get": "list"}), name='get-service-accounts-info'),
     path('<int:pk>', ServiceAccountsView.as_view({"patch": "partial_update"}),
          name='update-service-account'),
     path('<str:platform>', ServiceAccountsView.as_view({"get": "retrieve"}),
