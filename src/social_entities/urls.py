74daccd5183f6ba2a9f7893dbf0f2d4513c009f0
ilya3052 <ipopadin5@gmail.com>
2026-04-27 23:17:51 +0300

    добавление метода частичного обновления в разрешенные и изменение имен путей

--- src/social_entities/urls.py
+++ src/social_entities/urls.py
@@ -5,7 +5,7 @@ from social_entities.views import PlatformsView, GroupsView, CheckGroupAccessVie
 urlpatterns = [
     path('platforms/', PlatformsView.as_view({"get": "list", "post": "create"}), name='platforms'),
 
-    path('groups/', GroupsView.as_view({"get": "list", "post": "create"}), name='groups'),
-    path('groups/<int:pk>', GroupsView.as_view({"delete": "destroy"}), name='groups'),
+    path('groups/', GroupsView.as_view({"get": "list", "post": "create"}), name='groups-get-create'),
+    path('groups/<int:pk>', GroupsView.as_view({"delete": "destroy", "patch": "partial_update"}), name='groups-delete-update'),
     path('group/check-access/', CheckGroupAccessView.as_view(), name='check-group-access'),
 ]
