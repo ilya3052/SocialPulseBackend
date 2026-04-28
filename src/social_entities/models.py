1c85bc748d9cf2c27f8e23ea2753c3c45d3c65f5
ilya3052 <ipopadin5@gmail.com>
2026-04-28 22:57:53 +0300

    добавление слага группы

--- src/social_entities/models.py
+++ src/social_entities/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 from social_pulse import settings
 
@@ -21,10 +22,17 @@ class Group(models.Model):
     link = models.CharField(max_length=256)
     external_id = models.BigIntegerField(db_index=True)
     added_at = models.DateTimeField(default=default_expires_at)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+
     platform = models.ForeignKey('Platform', on_delete=models.CASCADE)
     user = models.ManyToManyField('users.CustomUser')
     service_account = models.ForeignKey('service_accounts.ServiceAccount', on_delete=models.SET_NULL, null=True,
                                         related_name='groups')
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
 
 
 class Platform(models.Model):
