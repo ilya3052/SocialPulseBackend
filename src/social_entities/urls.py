6572012a95cdc93aebcabb73d24e8469f48c61f3
ilya3052 <ipopadin5@gmail.com>
2026-04-30 23:40:34 +0300

    добавление метода get в url для получения экземпляра группы

--- src/social_entities/urls.py
+++ src/social_entities/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('platforms/', PlatformsView.as_view({"get": "list", "post": "create"}), name='platforms'),
 
     path('groups/', GroupsView.as_view({"get": "list", "post": "create"}), name='groups-get-create'),
-    path('groups/<int:pk>', GroupsView.as_view({"delete": "destroy", "patch": "partial_update"}), name='groups-delete-update'),
+    path('groups/<int:pk>/', GroupsView.as_view(
+        {"get": "retrieve", "delete": "destroy", "patch": "partial_update"}
+    ),name='groups-retrieve-delete-update'),
     path('group/check-access/', CheckGroupAccessView.as_view(), name='check-group-access'),
 ]
