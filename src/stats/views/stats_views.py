c1428abfffc1effb97bcbc2b1d9839ec1af8b59a
ilya3052 <ipopadin5@gmail.com>
2026-05-07 18:50:45 +0300

    изменение типа класса с ModelViewSet на APIView с переопределением метода get

--- src/stats/views/stats_views.py
+++ src/stats/views/stats_views.py
@@ -1,7 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from stats.models import Snapshot, SnapshotStats, AbsoluteStats
+from common.config import ENCRYPTION_KEY
+from common.utils import decrypt
+from social_entities.utils import Platforms
+from stats.models import Snapshot, SnapshotStats, AbsoluteStats, BestPosts
 from stats.serializers import SnapshotSerializer, SnapshotStatsSerializer, AbsoluteStatsSerializer
+from stats.services import get_best_posts_info
 
 
 class SnapshotView(viewsets.ModelViewSet):
@@ -28,18 +35,28 @@ class AbsoluteStatsView(viewsets.ModelViewSet):
     lookup_field = 'group_id'
     queryset = AbsoluteStats.objects.all()
     serializer_class = AbsoluteStatsSerializer
+    permission_classes = [IsAuthenticated]
 
-class BestPostsView(viewsets.ModelViewSet):
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
 
-        exclude_fields_str = self.request.GET.get('exclude_fields')
-        exclude_fields = exclude_fields_str.split(',') if exclude_fields_str else []
+class BestPostsView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request, *args, **kwargs):
+        params = request.GET.dict()
+        group_id = self.kwargs.get('group_id')
+        platform = params.get('platform')
+        if not platform:
+            return Response({"error": "Не указана целевая платформа"}, status=status.HTTP_404_NOT_FOUND)
 
-        context['exclude_fields'] = exclude_fields
+        platform = Platforms(platform)
+        posts: BestPosts = BestPosts.objects.select_related('group__service_account__data').get(group_id=group_id)
+        data = posts.group.service_account.data
 
-        return context
+        options = {}
 
-    lookup_field = 'group_id'
-    queryset = BestPosts.objects.all()
-    serializer_class = BestPostsSerializer
+        if platform == Platforms.TG:
+            options['session_path'] = data.session_path
+        elif platform == Platforms.VK:
+            options['service_key'] = decrypt(data.service_key, ENCRYPTION_KEY)
+
+        best_posts_info = get_best_posts_info(posts, platform, **options)
+        return Response({**best_posts_info}, status=status.HTTP_200_OK)
