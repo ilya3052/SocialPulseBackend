cf51289a5b96f652acafc3cc39c5ad943f5dfd32
ilya3052 <ipopadin5@gmail.com>
2026-05-07 16:13:59 +0300

    добавление папки media для хранения медиа-контента (аватарок групп)

--- src/social_pulse/settings.py
+++ src/social_pulse/settings.py
@@ -21,6 +21,10 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
