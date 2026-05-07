035c6417684591ee4a216cbf602548779404446f
ilya3052 <ipopadin5@gmail.com>
2026-05-07 18:55:15 +0300

    изменение url под новый сериализатор

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from stats.views import BestPostsView
 
 urlpatterns = [
-    path('<int:group_id>/best/', BestPostsView.as_view({"get": "retrieve"}))
+    path('<int:group_id>/best/', BestPostsView.as_view())
 ]
