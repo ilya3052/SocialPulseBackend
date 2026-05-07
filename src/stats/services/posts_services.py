584c7e799866aff8d81596a68dbe1efc9f5d54c2
ilya3052 <ipopadin5@gmail.com>
2026-05-07 18:49:27 +0300

    создание функции получения подробной информации о лучших постах группы ВК

--- src/stats/services/posts_services.py
+++ src/stats/services/posts_services.py
@@ -1,18 +1,64 @@
+import requests
 from asgiref.sync import async_to_sync
 
 from social_entities.utils import Platforms
+from stats.models import BestPosts
 
 
 def get_best_posts_info(best_posts_obj, platform, **kwargs):
-    return get_posts_info_by_platform_functions.get(platform)(best_posts_obj, kwargs)
+    return get_posts_info_by_platform_functions.get(platform)(best_posts_obj, **kwargs)
 
 
-def get_vk_best_posts_info(best_post_obj, platform, **kwargs):
-    pass
+def get_vk_best_posts_info(best_post_obj: BestPosts, service_key):
+    posts_ids = best_post_obj.to_dict()
+
+    group = best_post_obj.group
+    group_id = group.external_id
+    posts_ids_with_group_id = ','.join(list(map(lambda item: f'-{group_id}_{item}', posts_ids.values())))
+
+    params = {
+        'posts': posts_ids_with_group_id,
+        'v': 5.199
+    }
+    headers = {
+        'Authorization': f'Bearer {service_key}',
+    }
+    req = requests.get(
+        'https://api.vk.ru/method/wall.getById',
+        headers=headers,
+        params=params
+    )
+    # добавить обработку ошибок
+
+    items = req.json().get('response').get('items')
+    items_by_id = {str(item.get('id')): item for item in items}
+    posts_data = {
+        "most_viewed": {},
+        "most_liked": {},
+        "most_commented": {},
+        "most_reposted": {},
+    }
+
+    for metric, post_id in posts_ids.items():
+        item = items_by_id.get(str(post_id))
+        if item:
+            item_id = item.get('id')
+            posts_data[metric] = {
+                'link': f'https://vk.ru/wall-{group_id}_{item_id}',
+                'text': item.get('text'),
+                'metrics': {
+                    "views": item.get('views', {}).get('count', 0),
+                    "likes": item.get('likes', {}).get('count', 0),
+                    "comments": item.get('comments', {}).get('count', 0),
+                    "reposts": item.get('reposts', {}).get('count', 0)
+                }
+            }
+
+    return posts_data
 
 
 @async_to_sync
-def get_tg_best_posts_info(best_post_obj, platform, **kwargs):
+async def get_tg_best_posts_info(best_post_obj, session_path):
     pass
 
 
