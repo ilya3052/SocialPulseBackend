80381514578f16a81ad7701ebea38154e4923b05
ilya3052 <ipopadin5@gmail.com>
2026-05-10 21:32:50 +0300

    исправление функции save для генерации уникальных слагов группам с одинаковым названием

--- src/social_entities/models.py
+++ src/social_entities/models.py
@@ -22,7 +22,7 @@ class Group(models.Model):
     name = models.CharField(max_length=128)
     link = models.CharField(max_length=256)
     external_id = models.BigIntegerField(db_index=True)
-    added_at = models.DateTimeField(default=default_expires_at)
+    added_at = models.DateTimeField(default=timezone.now)
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
     platform = models.ForeignKey('Platform', on_delete=models.CASCADE)
@@ -30,9 +30,16 @@ class Group(models.Model):
     service_account = models.ForeignKey('service_accounts.ServiceAccount', on_delete=models.SET_NULL, null=True,
                                         related_name='groups')
 
-    def save(self, *args, **kwargs):  # new
+    def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(unidecode(self.name))
+            base_slug = slugify(unidecode(self.name))
+            slug = base_slug
+            counter = 1
+            while self.__class__.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+
         return super().save(*args, **kwargs)
 
 
