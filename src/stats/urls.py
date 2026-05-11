a701944de2c8741d771bed7b56207215b068a5b6
ilya3052 <ipopadin5@gmail.com>
2026-05-11 17:36:49 +0300

    добавление эндпоинта для получения объектов снапшота и лучших постов

--- src/stats/urls.py
+++ src/stats/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from stats.views import BestPostsView
+from stats.views import BestPostsView, SnapshotView
 
 urlpatterns = [
-    path('<int:group_id>/best/', BestPostsView.as_view())
+    path('<int:group_id>/best/', BestPostsView.as_view()),
+    path('<int:group_id>/', SnapshotView.as_view({"get": "list"}))
 ]
