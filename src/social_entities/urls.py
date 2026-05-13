c343005ab98acfb88271153be7c5c2af4bd32291
ilya3052 <ipopadin5@gmail.com>
2026-05-13 16:35:42 +0300

    создание эндпоинта для операции сравнения групп

--- src/social_entities/urls.py
+++ src/social_entities/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from social_entities.views import PlatformsView, GroupsViewByID, CheckGroupAccessView, GroupsViewBySlug
+from social_entities.views import PlatformsView, GroupsViewByID, CheckGroupAccessView, GroupsViewBySlug, \
+    CompareGroupsView
 
 urlpatterns = [
     path('platforms/', PlatformsView.as_view({"get": "list", "post": "create"}), name='platforms'),
@@ -9,6 +10,7 @@ urlpatterns = [
     path('groups/<int:pk>/', GroupsViewByID.as_view(
         {"delete": "destroy", "patch": "partial_update"}
     ), name='groups-delete-update'),
+    path('groups/compare/', CompareGroupsView.as_view(), name='compare-groups'),
     path('groups/<str:slug>/', GroupsViewBySlug.as_view({"get": "retrieve"}), name='groups-retrieve'),
     path('group/check-access/', CheckGroupAccessView.as_view(), name='check-group-access'),
 ]
