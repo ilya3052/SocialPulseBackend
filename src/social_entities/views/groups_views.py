b22d7d284c66dd42fd615276ca8e7335adb5a13e
ilya3052 <ipopadin5@gmail.com>
2026-05-14 20:31:23 +0300

    удаление группы через отдельный сервис

--- src/social_entities/views/groups_views.py
+++ src/social_entities/views/groups_views.py
@@ -70,13 +70,8 @@ class GroupsViewByID(viewsets.ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         group = self.get_object()
         user = request.user
-        users_count = group.users.count()
-        if not group.users.filter(id=user.id).exists():
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        if users_count == 1:
-            return super().destroy(request, *args, **kwargs)
-        group.users.remove(user)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        status_code = delete_group(group, user)
+        return Response(status=status_code)
 
 
 class GroupsViewBySlug(mixins.RetrieveModelMixin, GenericViewSet):
