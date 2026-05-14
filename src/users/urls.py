80a74b9198653a78953f1c16a4df23f367c50830
ilya3052 <ipopadin5@gmail.com>
2026-05-14 20:41:56 +0300

    создание эндпоинта для операции отвязки социальных сетей

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from users.views import UserAPIRegistration, UserAPIView, UserChangePasswordView, UserSetPasswordView, \
-    UserSocialDataView
+    UserSocialDataView, UnbindSocialView
 
 urlpatterns = [
     path('register/', UserAPIRegistration.as_view(), name='register'),
 
     path(r'me/', UserAPIView.as_view(), name='me'),
-
+    path('undind-social/', UnbindSocialView.as_view(), name='unbind_social'),
     path('change-password/', UserChangePasswordView.as_view(), name='change_password'),
     path('set-password/', UserSetPasswordView.as_view(), name='set_password'),
     path('get-social/', UserSocialDataView.as_view(), name='user-social-data'),
